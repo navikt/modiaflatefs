42ca38d6a432bc5c5435e22f8b6bc6cb9636f5ef
Christian Finstad <christian.finstad@nav.no>
2017-08-14 12:41:51 +0200

    IE-fix

--- web/src/frontend/src/index.js
+++ web/src/frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // eslint-disable-line import/no-unresolved
 import App from './app';
+import './polyfill/polyfills';
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Med mock'); // eslint-disable-line no-console
