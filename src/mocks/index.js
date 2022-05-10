b3110e22cf13d4ff3aa70f72c11c86aba38b9fad
Nicklas Utgaard <nutgaard@gmail.com>
2022-05-10 09:58:40 +0200

    [KAIZEN-0] bump dependencies og oppsett
    oppdaterer bibliotekversjoner og litt oppsett slik at appen igjen kan bygges på github

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,5 +1,5 @@
-import FetchMock, { MiddlewareUtils } from "yet-another-fetch-mock";
-import { contentTypeMiddleware, failurerateMiddleware } from './utils';
+import FetchMock, {MiddlewareUtils} from "yet-another-fetch-mock";
+import {contentTypeMiddleware, failurerateMiddleware} from './utils';
 
 console.log('=========================='); // tslint:disable-line
 console.log('======== MED MOCK ========'); // tslint:disable-line
@@ -20,11 +20,11 @@ const me = {
     fornavn: 'Fornavn',
     etternavn: 'Ettersen',
     enheter: [
-        { enhetId: '0101', navn: 'NAV Tatooine' },
-        { enhetId: '0211', navn: 'NAV Naboo' },
-        { enhetId: '0709', navn: 'NAV Larvik' },
-        { enhetId: '0104', navn: 'NAV Moss' },
-        { enhetId: '0109', navn: 'NAV Med et skikkelig langt navn' }
+        {enhetId: '0101', navn: 'NAV Tatooine'},
+        {enhetId: '0211', navn: 'NAV Naboo'},
+        {enhetId: '0709', navn: 'NAV Larvik'},
+        {enhetId: '0104', navn: 'NAV Moss'},
+        {enhetId: '0109', navn: 'NAV Med et skikkelig langt navn'}
     ]
 };
 const aktivEnhet = {
@@ -32,6 +32,18 @@ const aktivEnhet = {
     aktivBruker: null
 };
 
-mock.get('/modiacontextholder/api/decorator', (req, res, ctx) => res(ctx.json(me)));
-mock.get('/modiacontextholder/api/context/aktivenhet', (req, res, ctx) => res(ctx.json(aktivEnhet)));
-mock.post('/modiacontextholder/api/context', (req, res, ctx) => res(ctx.json({})));
+mock.get('/modiacontextholder/api/decorator', (req, res, ctx) => res(
+    ctx.delay(1000),
+    ctx.json(me))
+);
+
+mock.get('/modiacontextholder/api/context/aktivenhet', (req, res, ctx) => res(
+    ctx.delay(1000),
+    ctx.json(aktivEnhet))
+);
+
+mock.post('/modiacontextholder/api/context', (req, res, ctx) => res(
+    ctx.delay(1000),
+    ctx.json({}))
+);
+
