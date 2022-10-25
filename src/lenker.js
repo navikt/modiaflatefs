51be3a2d1093ab034d8464fd5bf01e1600927366
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-25 14:51:18 +0200

    [KAIZEN-0] håndtere feil mot contextholder bedre
    ved feil mot contextholder fikk brukere tidligere en generisk 500-feil side,
    denne slo ut når axsys hadde trøbbel, og vi bør ta en vurdering på hvorvidt
    en "snillere" feilmelding lar seg bruke her.

--- src/lenker.js
+++ src/lenker.js
@@ -1,38 +1,45 @@
 import React from 'react';
 import PT from 'prop-types';
+import { erstattMiljoPlaceholder, erstattMiljoPlaceholderNaisIntern } from "./utils";
 
-function Lenker({ enhet, minoversikt, modia, rekrutteringsbistand, sykefravaer }) {
+function Lenker({ enhetId }) {
+    const modiaUrl = erstattMiljoPlaceholder('https://modapp{{miljoStreng}}.adeo.no/modiabrukerdialog');
+    const syfoUrl = erstattMiljoPlaceholderNaisIntern('https://syfooversikt.{{miljoStreng}}/enhet');
+    const portefoljeflateUrl = erstattMiljoPlaceholderNaisIntern('https://veilarbportefoljeflate.{{miljoStreng}}');
+    const rekrutteringsbistandUrl = erstattMiljoPlaceholderNaisIntern('https://rekrutteringsbistand.{{miljoStreng}}/');
+    const enhetQueryParam = enhetId ? `?enhet=${enhetId}` : '';
+
     return (
         <div className="lenker">
             <div className="lenker__kolonne">
                 <div className="lenker__celle">
-                    <a className="lenke lenke--frittstaende" href={modia}>
+                    <a className="lenke lenke--frittstaende" href={modiaUrl}>
                         Personoversikt
                     </a>
                 </div>
             </div>
             <div className="lenker__kolonne">
                 <div className="lenker__celle">
-                    <a className="lenke lenke--frittstaende" href={enhet}>
+                    <a className="lenke lenke--frittstaende" href={`${portefoljeflateUrl}/enhet${enhetQueryParam}`}>
                         Enhetens oversikt
                     </a>
                 </div>
                 <div className="lenker__celle">
-                    <a className="lenke lenke--frittstaende" href={minoversikt}>
+                    <a className="lenke lenke--frittstaende" href={`${portefoljeflateUrl}/portefolje${enhetQueryParam}`}>
                         Min oversikt
                     </a>
                 </div>
             </div>
             <div className="lenker__kolonne">
                 <div className="lenker__celle">
-                    <a className="lenke lenke--frittstaende" href={sykefravaer}>
+                    <a className="lenke lenke--frittstaende" href={syfoUrl}>
                         Sykefravær
                     </a>
                 </div>
             </div>
             <div className="lenker__kolonne">
                 <div className="lenker__celle">
-                    <a className="lenke lenke--frittstaende" href={rekrutteringsbistand}>
+                    <a className="lenke lenke--frittstaende" href={rekrutteringsbistandUrl}>
                         Rekrutteringsbistand
                     </a>
                 </div>
@@ -42,11 +49,7 @@ function Lenker({ enhet, minoversikt, modia, rekrutteringsbistand, sykefravaer }
 }
 
 Lenker.propTypes = {
-    enhet: PT.string.isRequired,
-    minoversikt: PT.string.isRequired,
-    modia: PT.string.isRequired,
-    rekrutteringsbistand: PT.string.isRequired,
-    sykefravaer: PT.string.isRequired
+    enhetId: PT.string,
 };
 
 export default Lenker;
